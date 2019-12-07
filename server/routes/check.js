63af07db7919c25f48a5fb16321ef40785b718fb
Byungchul <byung-chul@nate.com>
2019-12-07 16:56:34 +0900

    delete /check/info /check edit

--- server/routes/check.js
+++ server/routes/check.js
@@ -8,7 +8,7 @@ router.get('/', function (req, res, next) {
 	const company_id = req.query.company_id;
 
 	const query =
-        "SELECT * FROM deliverylist WHERE company_id = ?";
+        "SELECT * FROM deliverylist WHERE company_id = ? AND delivery_type != 2";
     database.query(query, [company_id])
         .then(function(results) {
             res.end(JSON.stringify(results));
@@ -17,7 +17,40 @@ router.get('/', function (req, res, next) {
 
 router.get('/info', function (req, res, next) {
 
-	res.render('index', { title: 'Express' });
+    const delivery_id = req.query.delivery_id;
+
+    var device = awsIot.device({
+        keyPath: './certs/8c7ea71244-private.pem.key',
+       certPath: './certs/8c7ea71244-certificate.pem.crt',
+         caPath: './certs/rootCA.key',
+       clientId: 'subscriber',
+           host: 'aksvt2aysg9zx-ats.iot.us-east-2.amazonaws.com'
+    });
+
+    const query =
+            "SELECT *  FROM deliverylist WHERE delivery_id = ?";
+        database.query(query, [delivery_id])
+            .then(function(results) {
+
+                var car_id = results[0].car_id;
+
+                device.
+                    on('connect', function() {
+                    console.log('connect');
+                    device.subscribe('Truck/1Ton/'+ car_id);
+                    });
+                
+                device.
+                    on('message', function(topic, payload) {
+                    console.log(topic, payload.toString());
+                    var payload_json = JSON.parse(payload.toString())
+                    
+                    device.end();
+					
+					res.json(payload_json);
+                
+                });
+        });
 
 });
 
@@ -37,7 +70,8 @@ router.get('/car', function (req, res, next) {
             "SELECT car_id FROM user WHERE id = ?";
         database.query(query, [id])
             .then(function(results) {
-                car_id = results[0].car_id;
+
+                var car_id = results[0].car_id;
 
                 device.
                     on('connect', function() {
